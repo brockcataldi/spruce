d3c69d2047f6c54d3eeb760e9338ec6474b92868
Brock <brock@offcenter.io>
2021-12-03 10:28:36 -0500

    Added editor testing functions

--- spruce-functions.php
+++ spruce-functions.php
@@ -33,3 +33,40 @@ function classes( array $classes = array(), bool $echo = true ) {
 		return esc_html( $valid );
 	}
 }
+
+/**
+ * Determining if the current screen is an editor screen,
+ * this is really just a wrapper for is_admin() currently.
+ *
+ * @see https://developer.wordpress.org/reference/functions/is_admin/
+ *
+ * @return bool whether or not the screen is an admin screen
+ */
+function is_editor() {
+	return is_admin();
+}
+
+/**
+ * Echoing or Returning either an href with link or an empty string.
+ * The whole point of this is to echo out an href, so that's why there are
+ * phpcs:disables
+ *
+ * @param string $url the url to echo or return.
+ * @param bool   $echo whether to echo or to return.
+ *
+ * @return string the string
+ */
+function href( string $url, bool $echo = true ) {
+
+	$string = ( is_editor() ) ? '' : sprintf( 'href="%s"', $link );
+
+	if ( true === $echo ) {
+		// phpcs:disable
+		echo $string;
+		// phpcs:enable
+	} else {
+		// phpcs:disable
+		return $string;
+		// phpcs:enable
+	}
+}
